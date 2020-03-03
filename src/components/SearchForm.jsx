2549369eb08a034102eaed7410d065f8cae4d200
Kingsley Mgbams <cmgbams@gmail.com>
2020-03-03 17:09:01 +0100

    modification to searchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,46 +1,37 @@
-import React, { Component } from "react";
-import { Form, FormControl, Button } from "react-bootstrap";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
 class SearchForm extends Component {
   state = {
-    search: "",
+    search: '',
     redirect: false,
   };
 
   handleChange = (event) => {
     const search = event.target.value;
     this.setState({ search });
-  };
+  }
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
     this.setState({ redirect: true });
-  };
+  }
+
   render = () => {
     const { search, redirect } = this.state;
-    // Working on redirection
 
     if (redirect) {
       return <Redirect to={`/search/${search}`} />;
     }
 
     return (
-      <Form inline>
-        <FormControl
-          type="text"
-          placeholder="Search"
-          className="mr-sm-2"
-          onChange={this.handleChange}
-          value={search}
-          onSubmit={this.handleSubmit}
-        />
-        <Button type="submit" variant="outline-success">
-          Search
-        </Button>
+      <Form inline onSubmit={this.handleSubmit}>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={this.handleChange} value={search} />
+        <Button type="submit" variant="outline-success">Search</Button>
       </Form>
     );
-  };
+  }
 }
 
 export default SearchForm;
